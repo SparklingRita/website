6128ea75765cbe9739a981c78b0b1c3f76460ed1
Rita Mendes <rcordeiromendes@gmail.com>
2021-03-22 00:15:02 +0000

    added anime.js animations and overall organization

--- main.js
+++ main.js
@@ -8,3 +8,50 @@ function toggleMenu () {
         toggleStatus = 1; 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    anime({
+        targets: '.hero h1',
+        translateX: [-500,0],
+        duration: 2500,
+        easing: 'easeOutExpo',
+        delay: 100,
+    });
+
+    anime ({
+        targets: '.hero p',
+        translateY: [-50,0],
+        duration: 2000,
+        easing: 'easeOutExpo',
+       opacity: [0,1]
+    })
+    
+    anime ({
+        targets: '.textAbout',
+        translateX: [-200,0],
+        duration: 2500,
+        easing: 'easeOutExpo',
+       opacity: [0,1]
+    })
+})
+
+
+/*
+NOT WORKING -> SAD
+document.addEventListener('DOMContentLoaded', () => {
+    //create waypoint
+    let waypoint = new Waypoint ({
+        element: document.querySelector('.textAbout'),
+        handler: function() {
+            anime({
+                targets: '.textAbout',
+                easing: 'easeOutExpo',
+                translateX: [-100,0],
+                opacity: [0,1],
+                delay: 300,
+            })
+        },
+        context: document.querySelectorAll('.aboutMe'),
+    })
+}) */
+   
